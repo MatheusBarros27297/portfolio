81e615af40c3319c9500b477a159d33794961564
Matheus <crispertax27297@gmail.com>
2025-06-11 19:38:50 -0400

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://avatars.githubusercontent.com/u/144473860?v=4" />
+    <Titulo fontSize={20}>Gian Souza</Titulo>
+  </aside>
+)
+
+export default Sidebar
